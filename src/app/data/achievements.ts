5257156b532c3e981572fa3e63894062108a8550
Pavan S Poojary <01pavan06@gmail.com>
2025-09-29 10:50:43 +0000

    add this to achivements

--- src/app/data/achievements.ts
+++ src/app/data/achievements.ts
@@ -23,4 +23,10 @@ export const achievements = [
     description: 'Seven students from SMVITM were selected for the prestigious IEEE Women in Engineering Scholarship by Quest Global.',
     image: 'achievement-4',
   },
+  {
+    title: 'Excellent Contribution to IEEE Mangalore Subsection',
+    year: '2023',
+    description: 'Awarded in recognition of excellent contributions to the IEEE Mangalore Subsection during 2023.',
+    image: 'achievement-5',
+  },
 ];
